0fab587ef66cb471b3bab2c8d0f7671415d2af9c
https://github.com/Anandp332 <anandp332@gmail.com>
2021-10-03 23:44:02 +0530

    Init

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+print("Hi")
+print('Hello')
+# " -> uses for print any words or senetnces
+# ' ->  uses for print any words or senetnces
+#print() -> displaying anything (any data)
+# for install vs code :  https://code.visualstudio.com/
+# for install anaconda : https://www.anaconda.com/products/individual
